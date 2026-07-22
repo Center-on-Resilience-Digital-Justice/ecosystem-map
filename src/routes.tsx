40733eb52bf766245d13cf5af6b32f13c62e2df2
Ngozi Harrison <ngozih@g.ucla.edu>
2026-07-23 05:12:02 +0900

    put incorrect name of repo in the config, fixed now

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,14 +2,12 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.tsx'
 import NotFound from './pages/NotFound.tsx'
 
-const baseURL = import.meta.env.BASE_URL
 
 export default function AppRoutes() {
     return (
             <Routes>
-                <Route path={`${baseURL}`} element={<Home />} />
-                <Route path={`${baseURL}*`} element={<NotFound />} />
-                <Route path={`${baseURL}home`} element={<Home />} />
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
     )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,5 @@ export default defineConfig({
     tailwindcss(),
 
   ],
-  base: "/ecosystem-viewer/"
+  base: "/ecosystem-map/"
 })
